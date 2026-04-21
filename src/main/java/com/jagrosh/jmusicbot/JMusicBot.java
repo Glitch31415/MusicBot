2512843b7dd40bfc9b387e211ee6456722de7be6
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2026-04-21 12:40:55 -0700

    test

--- src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
+++ src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
@@ -711,7 +711,7 @@ if (!new File("/home/glitch/hlcoop-sfx/" + message[0].toLowerCase() + ".wav").ex
                             var data = objectMapper.readValue(body, new TypeReference<Object[]>(){});
                             //Object[] firstArray = ((java.util.List<?>) data[0]).get(0);
                             //return (String) firstArray[0];
-							return (String) ((java.util.List<?>) data[0]).get(0);
+							return (String) ((java.util.List<?>) data[0]).get(0).get(0);
                         } catch (Exception e) {
                             e.printStackTrace();
                             return null;
