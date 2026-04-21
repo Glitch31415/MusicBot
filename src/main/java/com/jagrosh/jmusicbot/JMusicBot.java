d279b1ed0b60996e0752635f8a4149d180d5a468
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2026-04-21 13:32:57 -0700

    test

--- src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
+++ src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
@@ -709,8 +709,9 @@ if (!new File("/home/glitch/hlcoop-sfx/" + message[0].toLowerCase() + ".wav").ex
                             // The response is a nested array, parse accordingly
                             // Example: [[["translated text","original text",null,null,...]],null,"en",...]
                             var data = objectMapper.readValue(body, new TypeReference<Object[]>(){});
-                            Object[] firstArray = (Object[]) ((java.util.List<?>) data[0]).get(0);
-                            return (String)firstArray[0].toString();
+                            //Object[] firstArray = (Object[]) ((java.util.List<?>) data[0]).get(0);
+                            //return firstArray[0].toString();
+							return data[0].get(0).toString();
                         } catch (Exception e) {
                             e.printStackTrace();
                             return null;
